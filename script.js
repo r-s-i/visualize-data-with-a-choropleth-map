f9bda728ba9562ad72abbef2f65eafdd06fe31d9
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-03-06 15:04:34 +0100

    Fetched and used education data, 6 out of 12 tests passes

--- script.js
+++ script.js
@@ -7,19 +7,57 @@ const svg = d3
   .attr("width", width)
   .attr("height", height);
 
-fetch(
-  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
-)
-  .then((respons) => respons.json())
-  .then((data) => {
-    const counties = topojson.feature(data, data.objects.counties);
-    const path = d3.geoPath();
+const colors = [
+  "rgb(0, 255, 0)",
+  "rgb(45, 255, 102)",
+  "rgb(90, 255, 153)",
+  "rgb(135, 255, 204)",
+  "rgb(180, 255, 255)",
+  "rgb(210, 255, 255)",
+];
 
-    svg
-      .selectAll("path")
-      .data(counties.features)
-      .enter()
-      .append("path")
-      .attr("d", path)
-      .attr("class", "county");
-  });
+async function drawMap() {
+  const educationData = await fetch(
+    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
+  )
+    .then((respons) => respons.json())
+    .then((data) => data);
+
+  fetch(
+    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
+  )
+    .then((respons) => respons.json())
+    .then((data) => {
+      const counties = topojson.feature(data, data.objects.counties);
+      const path = d3.geoPath();
+
+      svg
+        .selectAll("path")
+        .data(counties.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("class", "county")
+        .style("fill", (d) => {
+          for (let i = 0; i < educationData.length; i++) {
+            if (d.id === educationData[i].fips) {
+              const bachelorsOrHigher = educationData[i].bachelorsOrHigher;
+              if (bachelorsOrHigher > 50) {
+                return colors[0];
+              } else if (bachelorsOrHigher > 40) {
+                return colors[1];
+              } else if (bachelorsOrHigher > 30) {
+                return colors[2];
+              } else if (bachelorsOrHigher > 20) {
+                return colors[3];
+              } else if (bachelorsOrHigher > 10) {
+                return colors[4];
+              } else {
+                return colors[5];
+              }
+            }
+          }
+        });
+    });
+}
+drawMap();
